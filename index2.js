1fb6f68e0e0e7baec7d31a90f12ff13ae075fcdb
kirildz <kirildz@protonmail.com>
2021-11-18 08:37:32 +0200

    refactored main flow.

--- index2.js
+++ index2.js
@@ -1,8 +1,12 @@
 import fs from 'fs/promises';
 import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { NamesGenerator } from './src/NamesGenerator.js';
 import getLinksForDownloadingAndUpdateHtml from './src/temp1.js';
 import { downloadData, saveData } from './src/downloads.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default async (url, folder) => {
     const urlInstance = new URL(url);
@@ -11,27 +15,35 @@ export default async (url, folder) => {
     const fileName = namesGeneratorInstance.getPageName();
     const folderName = namesGeneratorInstance.getFolderName();
 
-    downloadData(url)
-        .then((response) => {
-            const {
-                linksForDownloading,
-                updatedLinksNames,
-                updatedHtml,
-            } = getLinksForDownloadingAndUpdateHtml(response.data, urlInstance.origin, folderName);
+    console.log(folder);
 
-            // return {
-            //     linksForDownloading,
-            //     updatedLinksNames,
-            //     updatedHtml,
-            // };
-        })
-        .then(() => {
+    // let pageData;
 
-        })
-        .then(() => {
-
-        })
-        .catch((err) => {
-            console.error('This is global ERROR', err);
-        });
+    // downloadData(url)
+    //     .then((response) => {
+    //         pageData = getLinksForDownloadingAndUpdateHtml(response.data, urlInstance.origin, folderName);
+    //
+    //         return fs.mkdir(folderName);
+    //     })
+    //     .then(() => {
+    //         const {
+    //             linksForDownloading,
+    //             updatedLinksNames,
+    //             updatedHtml,
+    //         } = pageData;
+    //         const promise = linksForDownloading.map((link, index) => downloadData(link)
+    //             .then((responseData) => {
+    //                 const { data } = responseData;
+    //
+    //                 return saveData(path.join(folderName, updatedLinksNames[index]), data);
+    //             }));
+    //
+    //         const promise1 = saveData(fileName, updatedHtml);
+    //
+    //         return Promise.all(promise.concat(promise1));
+    //     })
+    //     .then(() => console.log(path.join(__dirname, fileName)))
+    //     .catch((err) => {
+    //         console.error('This is global ERROR', err);
+    //     });
 };
